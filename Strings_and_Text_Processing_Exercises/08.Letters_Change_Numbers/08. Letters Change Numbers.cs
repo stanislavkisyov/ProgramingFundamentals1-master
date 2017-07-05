1840a5eab45f928ab1bfcd88fb695a7167029fd3
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-07-05 17:53:43 +0300

    add new problem

---
+++ Strings_and_Text_Processing_Exercises/08.Letters_Change_Numbers/08. Letters Change Numbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Letters_Change_Numbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
