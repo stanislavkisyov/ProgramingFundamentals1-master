00a9823d0898ae88a9b0e5de93376ca316bf979a
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-07-05 17:24:27 +0300

    test

---
+++ 04.Split_by_Word_Casing/04. Split by Word Casing.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Split_by_Word_Casing
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var separator = new char[] { ',', ':', ';', '.', '!', '(', ')', '"', '\'', '\\', '/', '[', ']', ' ' };
+
+            string[] input = Console.ReadLine().Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            var lowerCaseWords = new List<string>();
+            var upperCasewords = new List<string>();
+            var mixedCaseWords = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int lowerCase = 0;
+                int upperCase = 0;
+                int mixedCase = 0;
+
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    if (input[i][j] >= 97 && input[i][j] <= 122)
+                    {
+                        lowerCase++;
+                    }
+                    else if (input[i][j] >= 65 && input[i][j] <= 90)
+                    {
+                        upperCase++;
+                    }
+                    else
+                    {
+                        mixedCase++;
+                    }
+                }
+
+                if (lowerCase == 0 && upperCase > 0 && mixedCase == 0)
+                {
+                    upperCasewords.Add(input[i]);
+                }
+                else if(lowerCase > 0 && upperCase == 0 && mixedCase == 0)
+                {
+                    lowerCaseWords.Add(input[i]);
+                }
+                else if(lowerCase >= 0 && upperCase >= 0 && mixedCase >= 0)
+                {
+                    mixedCaseWords.Add(input[i]);
+                }
+
+            }
+
+            Console.WriteLine("Lower-case: {0}", string.Join(", ", lowerCaseWords));
+            Console.WriteLine($"Mixed-case: {string.Join(", ", mixedCaseWords)}");
+            Console.WriteLine($"Upper-case: {string.Join(", ", upperCasewords)}");
+        }
+    }
+}
---
+++ 07.Count_Numbers/07. Count Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Numbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            var count = new int[nums.Max() + 1];
+            foreach (var num in nums)
+            {
+                count[num]++;
+            }
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (count[i]> 0)
+                {
+                    Console.WriteLine($"{i} -> {count[i]}");
+                }
+            }
+        }
+    }
+}
